2969457a45804fa2c442baa53848f813beb4061e
Dharan Costa <dharan.costa@iteris.com.br>
2022-09-16 11:58:40 -0300

    SOLID: Single Responsibility Principle

---
+++ src/br/com/alura/rh/service/ValidacaoPeriodicidade.java
@@ -0,0 +1,19 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ValidacaoPeriodicidade implements ValidacaoReajuste{
+    public void validar(Funcionario funcionario, BigDecimal aumento){
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDeseUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste,dataAtual);
+        if (mesesDeseUltimoReajuste < 6){
+            throw new ValidacaoException("Reajuste nao pode ser feito em menos de 6 meses do último reajuste!");
+        }
+    }
+}
